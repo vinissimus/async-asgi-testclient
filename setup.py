ab501e33bb5438bd6a6017354f4e488c09cd052a
Jordi Masip <jordi@masip.cat>
2019-06-17 14:06:55 +0200

    Unpinned dependencies

--- setup.py
+++ setup.py
@@ -38,6 +38,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/vinissimus/async-asgi-testclient",
-    version="1.0.0",
+    version="1.0.1",
     zip_safe=False,
 )
