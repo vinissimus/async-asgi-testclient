777fd8807eeb7568c8c7bf8f2403c2483aef7d7a
Daniel <dmanchon@gmail.com>
2019-06-17 11:21:06 +0200

    version bump

--- setup.py
+++ setup.py
@@ -38,6 +38,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/vinissimus/async-asgi-testclient",
-    version="0.2.3",
+    version="1.0.0",
     zip_safe=False,
 )
