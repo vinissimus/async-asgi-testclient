21f8853989acbf34c936a27f9db540888c65a94e
Daniel <dmanchon@gmail.com>
2019-06-17 11:01:25 +0200

    Websocket client

--- setup.py
+++ setup.py
@@ -37,6 +37,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/vinissimus/async-asgi-testclient",
-    version="0.2.2",
+    version="0.2.3",
     zip_safe=False,
 )
