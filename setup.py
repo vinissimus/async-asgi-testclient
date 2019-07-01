dfcb9a17c50f621a869e1775ff69c615a77c8e42
Jordi Masip <masipcat@users.noreply.github.com>
2019-07-01 09:47:32 +0200

    Fix response with multime cookies (#10)

--- setup.py
+++ setup.py
@@ -38,6 +38,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/vinissimus/async-asgi-testclient",
-    version="1.0.2",
+    version="1.0.3",
     zip_safe=False,
 )
