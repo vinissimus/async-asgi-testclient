21a8c5ff845228411cd3b81fa649b191f6321406
Jordi Masip <masipcat@users.noreply.github.com>
2019-12-07 11:48:12 +0000

    Quote query_string by default (#22)

--- setup.py
+++ setup.py
@@ -39,6 +39,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/vinissimus/async-asgi-testclient",
-    version="1.2.1",
+    version="1.2.2",
     zip_safe=False,
 )
