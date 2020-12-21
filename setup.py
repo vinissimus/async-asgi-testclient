0ae2fd598a8be72de96e5ce1448871a743e5b829
Kostya Leschenko <kostya.leschenko@datarobot.com>
2020-12-21 17:43:47 +0200

    Add support for Python 3.9

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     description="Async client for testing ASGI web applications",
     install_requires=requirements,
