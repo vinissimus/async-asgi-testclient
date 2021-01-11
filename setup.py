d1b93fa28f17518920fb9f03583bbfcb74ea8ae4
Jordi Masip <jordi@masip.cat>
2021-01-11 13:25:55 +0100

    Bump v1.4.5

--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/vinissimus/async-asgi-testclient",
-    version="1.4.4",
+    version="1.4.5",
     zip_safe=False,
 )
